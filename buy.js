90d693339b741d7fdc9aeaae6a5c70ae247976f8
mickfenneck <michele.sordo@gmail.com>
2018-04-13 21:11:00 +0200

    italian

--- buy.js
+++ buy.js
@@ -12,3 +12,18 @@ function bookTravelBox() {
   document.getElementById("form-subject").setAttribute('value','BOOK TRAVEL BOX');
   document.getElementById("form-message").value = 'Hi guys, I am interested in the Travel Box and this is my email!';
 }
+
+function bookBakuIT() {
+  document.getElementById("form-subject").setAttribute('value','PRENOTAZIONE BAKU');
+  document.getElementById("form-message").value = 'Ciao ragazzi, sono interessato a comprare Baku e questa è la mia email!';
+}
+
+function bookCoverIT() {
+  document.getElementById("form-subject").setAttribute('value','PRENOTAZIONE COVER');
+  document.getElementById("form-message").value = 'Ciao ragazzi, sono interessato a comprare la Cover e questa è la mia email!';
+}
+
+function bookTravelBoxIT() {
+  document.getElementById("form-subject").setAttribute('value','PRENOTAZIONE SCATOLA DEL VIAGGIATORE');
+  document.getElementById("form-message").value = 'Ciao ragazzi, sono interessato a comprare la Scatola del Viaggiatore e questa è la mia email!';
+}
